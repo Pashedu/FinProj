6aed38c689d286e9816bdcd2070f94b0fa7af87c
Pashedu <pashed@gmail.com>
2017-05-16 16:49:05 +0300

    mod for php 5.4

--- src/Softgroup/FinalBundle/Form/MessageType.php
+++ src/Softgroup/FinalBundle/Form/MessageType.php
@@ -17,9 +17,9 @@ class MessageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('messagetext')
-            ->add('plainPassword', RepeatedType::class, array(
+            ->add('plainPassword', 'Symfony\Component\Form\Extension\Core\Type\RepeatedType', array(
 
-                'type' => PasswordType::class,
+                'type' => 'Symfony\Component\Form\Extension\Core\Type\PasswordType',
                 'invalid_message' => 'The password fields must match.',
                 'required' => false,
                 'attr' => array('autocomplete' => 'off'),
@@ -27,7 +27,7 @@ class MessageType extends AbstractType
                 'second_options' => array('label' => 'Repeat Password'),
             ))
             ->add('email');
-        $builder->add('TTL', ChoiceType::class, array(
+        $builder->add('TTL', 'Symfony\Component\Form\Extension\Core\ChoiceList\ChoiceList', array(
             'choice_list' => new ChoiceList(
                 array('0','1','2','3','4','5'),
                 array('set TTL','1 hour','2 hours','1 day','3 days','1 week')
